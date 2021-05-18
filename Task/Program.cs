78e0f151fb94641ff1528acf9dbf172d1de2ef30
Srujana Jami <srujana.jami@innRoad.com>
2021-05-18 12:06:55 +0530

    Add project files.

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 5, 7, 9, 7, 13 };
+            int[] arr2 = new int[] { 5, -7, 10, 7, 34, -5 };
+
+            for (int i=0;i<arr1.Length;i++)
+            {
+                for (int j=0;j<arr2.Length;j++)
+                {
+                    int sum = 0;
+                    sum = arr1[i] + arr2[j];
+                    if (sum == 0)
+                    {
+                        Console.WriteLine(i + "," + j);
+                    }
+                }
+            }
+           
+        }
+    }
+}
+    
